dc5ea26fa89f67aecd8a8f623c5b3fe348547692
Arnav Saxena <arnavsaxena17@gmail.com>
2024-08-29 11:19:48 +0530

    chnages

--- packages/twenty-server/src/engine/core-modules/arx-chat/services/whatsapp-api/facebook-whatsapp/facebook-whatsapp-api.ts
+++ packages/twenty-server/src/engine/core-modules/arx-chat/services/whatsapp-api/facebook-whatsapp/facebook-whatsapp-api.ts
@@ -34,7 +34,7 @@ if (process.env.FACEBOOK_WHATSAPP_PERMANENT_API) {
   whatsappAPIToken = process.env.FACEBOOK_WHATSAPP_API_TOKEN;
 }
 
-const templates = ['hello_world', 'recruitment','application'];
+const templates = ['hello_world', 'recruitment','application','application02'];
 export class FacebookWhatsappChatApi {
   async uploadAndSendFileToWhatsApp(attachmentMessage: allDataObjects.AttachmentMessageObject) {
     console.log('Send file');
@@ -516,7 +516,7 @@ export class FacebookWhatsappChatApi {
         console.log('This is the template api message to send in whatappUpdateMessageObj.phoneNumberFrom, ', whatappUpdateMessageObj.phoneNumberFrom);
         const sendTemplateMessageObj = {
           recipient: whatappUpdateMessageObj.phoneNumberTo.replace('+', ''),
-          template_name: templates[2],
+          template_name: templates[3],
           candidateFirstName: whatappUpdateMessageObj.candidateFirstName,
           recruiterName: allDataObjects.recruiterProfile.name,
           recruiterJobTitle: allDataObjects.recruiterProfile.job_title,
