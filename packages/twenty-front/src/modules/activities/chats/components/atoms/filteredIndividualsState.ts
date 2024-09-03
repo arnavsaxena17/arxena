ec67b547893705e268cbaed4bb13025cb67c723f
Arnav Saxena <arnavsaxena17@gmail.com>
2024-09-03 15:35:34 +0530

    changes

--- packages/twenty-front/src/modules/activities/chats/components/atoms/filteredIndividualsState.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from 'recoil';
-import * as frontChatTypes from "../../types/front-chat-types";
-
-export const filteredIndividualsState = atom<frontChatTypes.PersonNode[]>({
-  key: 'filteredIndividualsState',
-  default: [],
-});
