b18e1bad359ae7f670ff8afff2c8a19a404dda43
Arnav Saxena <arnavsaxena17@gmail.com>
2024-09-26 23:13:15 +0530

    chnages

--- build_twenty_push_to_production.sh
+++ build_twenty_push_to_production.sh
@@ -2,5 +2,5 @@ git add .
 git commit -m "chnages"
 git push
 echo "Commits Pushed"
-ssh -i "~/arx-analytics-key.pem" ubuntu@ec2-184-72-74-33.compute-1.amazonaws.com ./push_to_production.sh
+ssh -i "~/arx-analytics-key.pem" ubuntu@ec2-184-72-74-33.compute-1.amazonaws.com ./build_app_in_new_instance.sh
 echo "SSHed into Remote"
