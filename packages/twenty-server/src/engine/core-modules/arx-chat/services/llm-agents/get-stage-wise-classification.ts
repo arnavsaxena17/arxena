259c47082935050f7b02aec5a6541ebbf8c08a07
Arnav Saxena <arnavsaxena17@gmail.com>
2024-10-25 11:05:35 +0530

    changes

--- packages/twenty-server/src/engine/core-modules/arx-chat/services/llm-agents/get-stage-wise-classification.ts
+++ packages/twenty-server/src/engine/core-modules/arx-chat/services/llm-agents/get-stage-wise-classification.ts
@@ -10,44 +10,27 @@ import Anthropic from '@anthropic-ai/sdk';
 import { zodResponseFormat } from "openai/helpers/zod";
 import { z } from "zod";
 
+import { LLMProviders } from './llm-agents';
 
 
-
+async function updateMostRecentMessagesBasedOnNewSystemPrompt(mostRecentMessageArr: allDataObjects.ChatHistoryItem[], newSystemPrompt: string) {
+  mostRecentMessageArr[0] = { role: 'system', content: newSystemPrompt };
+  return mostRecentMessageArr;
+}
 export async function getStageOfTheConversation(personNode:allDataObjects.PersonNode, mostRecentMessageArr: allDataObjects.ChatHistoryItem[], engagementType: 'remind' | 'engage', processorType: string) {
     let stage: string | null;
     console.log('Engagement Type::', engagementType);
-    if (engagementType === 'remind') {
-      console.log('Engagement type is reminder, so will try to get the stage from the last message');
-      const messagesWithTimeStamp = personNode.candidates.edges[0].node.whatsappMessages.edges.map(edge => {
-          return {
-            role: edge.node.name === 'candidateMessage' ? 'user' : 'assistant',
-            content: 'TimeStamp: ' + edge.node.createdAt + ' Message: ' + edge.node.message,
-          };
-        }).reverse();
-      const checkReminderPrompt = await new ToolsForAgents().getTimeManagementPrompt(personNode);
-      messagesWithTimeStamp.unshift({ role: 'system', content: checkReminderPrompt });
-      console.log('got here to with the checkReminderPrompt prompt', checkReminderPrompt);
-      // @ts-ignore
-      const response = await this.openAIclient.chat.completions.create({ model: modelName, messages: messagesWithTimeStamp });
-      stage = response.choices[0].message.content;
-      console.log('This is the stage that is determined by the model:', stage);
-
-      console.log('MessagesWithTimeStamp:::', messagesWithTimeStamp);
-      console.log('mostRecentMessageArr::', mostRecentMessageArr);
-      stage = 'remind_candidate';
-      console.log('This is the stage here:', stage);
-    } else {
-      console.log('got here to get the stage of the conversation');
+    console.log('got here to get the stage of the conversation');
       const stagePrompt = await new ToolsForAgents().getStagePrompt();
       // console.log('got here to with the stage prompt', stagePrompt);
-      const updatedMostRecentMessagesBasedOnNewSystemPrompt = await this.updateMostRecentMessagesBasedOnNewSystemPrompt(mostRecentMessageArr, stagePrompt);
+      const updatedMostRecentMessagesBasedOnNewSystemPrompt = await updateMostRecentMessagesBasedOnNewSystemPrompt(mostRecentMessageArr, stagePrompt);
       console.log('Got the updated recement messages for stage prompt:', updatedMostRecentMessagesBasedOnNewSystemPrompt);
       // @ts-ignore
-      const response = await this.openAIclient.chat.completions.create({ model: modelName, messages: updatedMostRecentMessagesBasedOnNewSystemPrompt });
+      const response = await new LLMProviders().openAIclient.chat.completions.create({ model: modelName, messages: updatedMostRecentMessagesBasedOnNewSystemPrompt });
       console.log("This is the stage that is arrived at:::", response.choices[0].message.content)
       stage = response.choices[0].message.content ?? '1';
       console.log('This the stage that is determined by the model:', response.choices[0].message.content, '  Stage::', stage, '  processorType::', processorType);
-    }
+    // }
     console.log('This is the stage that is arrived at:', stage);
     return stage;
   }
