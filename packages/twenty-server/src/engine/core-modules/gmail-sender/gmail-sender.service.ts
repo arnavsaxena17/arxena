3b4a1249b9f71f0d82145c837a0070ddd15f3daf
Arnav Saxena <arnavsaxena17@gmail.com>
2024-11-18 19:25:27 +0530

    changes

--- packages/twenty-server/src/engine/core-modules/gmail-sender/gmail-sender.service.ts
+++ packages/twenty-server/src/engine/core-modules/gmail-sender/gmail-sender.service.ts
@@ -17,7 +17,8 @@ const SCOPES = [
   "https://www.googleapis.com/auth/gmail.send",
   'https://www.googleapis.com/auth/gmail.modify',
   'https://www.googleapis.com/auth/gmail.compose',
-  'https://www.googleapis.com/auth/gmail.drafts'
+  'https://www.googleapis.com/auth/gmail.drafts',
+  'https://mail.google.com',
 ];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
