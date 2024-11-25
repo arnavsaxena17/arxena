2521132717fdbaacab9b63b2933e7da848659225
Arnav Saxena <arnavsaxena17@gmail.com>
2024-11-25 14:30:00 +0530

    changes

--- packages/twenty-front/src/pages/impersonate/ImpersonateEffect.tsx
+++ packages/twenty-front/src/pages/impersonate/ImpersonateEffect.tsx
@@ -48,6 +48,7 @@ export const ImpersonateEffect = () => {
   }, [userId, impersonate, setCurrentUser, setTokenPair]);
 
   useEffect(() => {
+    console.log('ImpersonateEffect', userId, currentUser, isLogged);
     if (
       isLogged &&
       currentUser?.canImpersonate === true &&
