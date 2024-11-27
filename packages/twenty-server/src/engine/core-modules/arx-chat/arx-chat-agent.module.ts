03cdbad9c4f543e707ba487641b74434435b83db
Arnav Saxena <arnavsaxena17@gmail.com>
2024-11-27 16:29:15 +0530

    changes

--- packages/twenty-server/src/engine/core-modules/arx-chat/arx-chat-agent.module.ts
+++ packages/twenty-server/src/engine/core-modules/arx-chat/arx-chat-agent.module.ts
@@ -1,15 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeORMModule } from 'src/database/typeorm/typeorm.module';
 
 import { ArxChatEndpoint, UpdateChatEndpoint, WhatsappControllers, WhatsappWebhook,WhatsappTestAPI,TwilioControllers, GoogleControllers } from 'src/engine/core-modules/arx-chat/arx-chat-agent.controller';
 import { TasksService } from 'src/engine/core-modules/arx-chat/services/candidate-engagement/scheduling-agent';
 import { AuthModule } from 'src/engine/core-modules/auth/auth.module';
 import { GoogleCalendarController } from 'src/engine/core-modules/calendar-events/google-calendar.controller';
 import { GoogleCalendarModule } from 'src/engine/core-modules/calendar-events/google-calendar.module';
+import { Workspace } from '../workspace/workspace.entity';
+import { DataSourceEntity } from 'src/engine/metadata-modules/data-source/data-source.entity';
+import { FeatureFlagEntity } from '../feature-flag/feature-flag.entity';
+import { WorkspaceDataSourceService } from 'src/engine/workspace-datasource/workspace-datasource.service';
+import { DataSourceModule } from 'src/engine/metadata-modules/data-source/data-source.module'; // Add this import
+
 
 @Module({
-  imports: [AuthModule, GoogleCalendarModule],
+  
+  imports: [AuthModule,  GoogleCalendarModule,     DataSourceModule, TypeORMModule, TypeOrmModule.forFeature([Workspace, FeatureFlagEntity], 'core'), TypeOrmModule.forFeature([DataSourceEntity], 'metadata') ],
   controllers: [ArxChatEndpoint, UpdateChatEndpoint, WhatsappWebhook, WhatsappControllers,WhatsappTestAPI,TwilioControllers, GoogleControllers],
-  providers: [TasksService],
+  providers: [TasksService, WorkspaceDataSourceService],
   exports: [],
 })
 export class ArxChatAgentModule {}
--- packages/twenty-server/src/engine/core-modules/arx-chat/services/candidate-engagement/scheduling-agent.ts
+++ packages/twenty-server/src/engine/core-modules/arx-chat/services/candidate-engagement/scheduling-agent.ts
@@ -1,6 +1,13 @@
 import CandidateEngagementArx from '../candidate-engagement/check-candidate-engagement';
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { Repository, In, EntityManager } from 'typeorm';
+import { EnvironmentService } from 'src/engine/integrations/environment/environment.service';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Workspace } from 'src/engine/core-modules/workspace/workspace.entity';
+import { DataSourceEntity } from 'src/engine/metadata-modules/data-source/data-source.entity';
+import { WorkspaceDataSourceService } from 'src/engine/workspace-datasource/workspace-datasource.service';
 
 let timeScheduleCron:string
 console.log("Current Environment Is:", process.env.NODE_ENV)
@@ -17,11 +24,21 @@ else{
 
 @Injectable()
 export class TasksService {
-  private readonly logger = new Logger(TasksService.name);
-  @Cron(timeScheduleCron)
+  constructor(
+
+  @InjectRepository(Workspace, 'core')
+  private readonly workspaceRepository: Repository<Workspace>,
+  @InjectRepository(DataSourceEntity, 'metadata')
+  private readonly dataSourceRepository: Repository<DataSourceEntity>,
+  private readonly environmentService: EnvironmentService,
+  private readonly workspaceDataSourceService: WorkspaceDataSourceService,
+
+) {}
+@Cron(timeScheduleCron)
   async handleCron() {
     // this.logger.log("Evert 5 seconds check Candidate Engagement is called");
     console.log("Starting CRON CYCLE")
+    await this.runWorkspaceServiceCandidateEngagement()
     if (process.env.RUN_SCHEDULER === 'true') {
       console.log("Checking Engagement")
       await new CandidateEngagementArx().checkCandidateEngagement();
@@ -30,6 +47,58 @@ export class TasksService {
     }
     console.log("ENDING CRON CYCLE")
   }
+
+  async runWorkspaceServiceCandidateEngagement(    transactionManager?: EntityManager  ){
+    console.log("workspaceRepository:", this.workspaceRepository)
+    console.log("this.environmentService:", this.environmentService)
+    const workspaceIds = (
+      await this.workspaceRepository.find({
+        where: this.environmentService.get('IS_BILLING_ENABLED')
+          ? {
+              subscriptionStatus: In(['active', 'trialing', 'past_due']),
+            }
+          : {},
+        select: ['id'],
+      })
+    ).map((workspace) => workspace.id);
+    console.log("workspaceIds::", workspaceIds)
+  
+  
+    const dataSources = await this.dataSourceRepository.find({
+      where: {
+        workspaceId: In(workspaceIds),
+      },
+    });
+
+    const workspaceIdsWithDataSources = new Set(
+      dataSources.map((dataSource) => dataSource.workspaceId),
+    );
+
+    console.log("workspaceIdsWithDataSources::", workspaceIdsWithDataSources)
+    console.log("dataSources::", dataSources)
+  
+    for (const workspaceId of workspaceIdsWithDataSources) {
+      
+      const dataSourceSchema =
+        this.workspaceDataSourceService.getSchemaName(workspaceId);
+        console.log("dataSourceSchema::", dataSourceSchema)
+
+        const lastPosition = await this.workspaceDataSourceService.executeRawQuery(
+          `SELECT MAX(position) FROM ${dataSourceSchema}.person`,
+          [],
+          workspaceId,
+          transactionManager,
+        );
+        console.log("lastPosition::", lastPosition, "for workspace ID:", workspaceId, "for dataSourceSchema:", dataSourceSchema)
+    }
+
+
+
+
+  }
+
+  
+
 }
 
 
