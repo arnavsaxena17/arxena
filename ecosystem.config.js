b43c5bb95aa862eddc3c3639c857111817919f38
Arnav Saxena <arnavsaxena17@gmail.com>
2025-02-25 14:05:59 +0530

    changes

---
+++ ecosystem.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    apps: [
+    {
+        name: "twenty-shared",
+        script: "./pm2_start_shared.sh",
+        watch: false,
+    },
+      {
+        name: "twenty-server",
+        script: "./pm2_start.sh",
+        watch: false,
+        env: {
+          PORT: 3000,
+          NODE_ENV: "production",
+        },
+        env_production: {
+          PORT: 3000,
+          NODE_ENV: "production",
+        },
+      },
+      {
+        name: "twenty-worker",
+        script: "./pm2_start_worker.sh",
+        watch: false,
+      },
+    ],
+  };
+  cat
