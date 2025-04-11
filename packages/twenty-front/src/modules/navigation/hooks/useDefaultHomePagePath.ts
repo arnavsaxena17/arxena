a3fb37d7ece7278daf1b07dfe026b256dc6d9f09
Arnav Saxena <arnavsaxena17@gmail.com>
2025-04-11 16:28:15 +0530

    changes

--- packages/twenty-front/src/modules/navigation/hooks/useDefaultHomePagePath.ts
+++ packages/twenty-front/src/modules/navigation/hooks/useDefaultHomePagePath.ts
@@ -81,7 +81,7 @@ export const useDefaultHomePagePath = () => {
 
     // const namePlural = defaultObjectPathInfo.objectMetadataItem?.namePlural;
     // const viewId = defaultObjectPathInfo.view?.id;
-    return AppPath.Dashboard;
+    return AppPath.Chats;
 
     // return getAppPath(
     //   AppPath.RecordIndexPage,
