2a3a745754f7c1e2fa3bff1709d08c38da9e6e20
Arnav Saxena <arnavsaxena17@gmail.com>
2025-05-01 15:20:48 +0530

    changes

--- packages/twenty-front/src/modules/app/components/AppRouterProviders.tsx
+++ packages/twenty-front/src/modules/app/components/AppRouterProviders.tsx
@@ -23,7 +23,7 @@ import { PageFavicon } from '@/ui/utilities/page-favicon/components/PageFavicon'
 import { PageTitle } from '@/ui/utilities/page-title/components/PageTitle';
 import { UserProvider } from '@/users/components/UserProvider';
 import { UserProviderEffect } from '@/users/components/UserProviderEffect';
-import { WebSocketProvider } from '@/websocket-context/WebsocketContext';
+import { WebSocketProvider } from '@/websocket-context/WebSocketContextProvider';
 import { WorkspaceProviderEffect } from '@/workspace/components/WorkspaceProviderEffect';
 import { StrictMode } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
