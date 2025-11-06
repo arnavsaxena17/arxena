a3cf4c9feb95bd3e8d78e78ec4124620e3786673
Arnav Saxena <arnavsaxena17@gmail.com>
2025-11-06 16:24:13 +0530

    changes

--- packages/twenty-server/src/engine/core-modules/candidate-sourcing/controllers/candidate-sourcing.controller.ts
+++ packages/twenty-server/src/engine/core-modules/candidate-sourcing/controllers/candidate-sourcing.controller.ts
@@ -948,6 +948,7 @@ export class CandidateSourcingController {
         apiToken,
       );
       jobs = responseFromGetAllJobs?.data?.data?.jobs?.edges || [];
+      console.log('Number of jobs in get-all-jobs:', jobs.length);
     } catch (error) {
       console.error('Error fetching jobs in get-all-jobs:', error);
       jobs = [];
