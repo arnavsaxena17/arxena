1d0473ffa59b0c074e0ba342253188632bd4c5a8
Arnav Saxena <arnavsaxena17@gmail.com>
2026-04-28 16:32:57 +0530

    cahnges

--- packages/twenty-shared/src/workspace/utils/isWorkspaceActiveOrSuspended.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isWorkspaceActiveOrSuspended = void 0;
-const isWorkspaceActiveOrSuspended = (workspace) => {
-    return ((workspace === null || workspace === void 0 ? void 0 : workspace.activationStatus) === "ACTIVE" ||
-        (workspace === null || workspace === void 0 ? void 0 : workspace.activationStatus) === "SUSPENDED");
-};
-exports.isWorkspaceActiveOrSuspended = isWorkspaceActiveOrSuspended;
-//# sourceMappingURL=isWorkspaceActiveOrSuspended.js.map
